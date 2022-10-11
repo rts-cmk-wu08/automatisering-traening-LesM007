e9377717ba0161fe2378d450a5d6646de7f0d6ea
LesM007 <77612956+LesM007@users.noreply.github.com>
2022-10-11 12:46:11 +0200

    commit community

--- src/components/comSection.js
+++ src/components/comSection.js
@@ -1,2 +1,20 @@
 import {sites} from "../data.js"
-console.log(sites.headline)
+import comArticle from "./comArticle.js"
+
+
+console.log(sites.places)
+
+let comSection = function() {
+
+    let element = document.createElement('div')
+    element.classList.add("community-article")
+
+    sites.places.forEach(site => {
+
+        element.append(comArticle(site))
+        })
+
+    return element
+}
+
+export default comSection
--- src/components/faciArticle.js
+++ src/components/faciArticle.js
@@ -8,7 +8,7 @@ let faciArticle = function(facility) {
         <img class="faci-article__icon"src="${facility.icon}" alt="icon">
         <h1 class="faci-article__heading">${facility.headline}</h1>
         <p class="faci-article__text">${facility.text}</p>
-        <span class="faci-article__span">Show me more</span>
+        <span class="faci-article__link">Show me more</span>
         `
 
     return element
